f95b16a419dc0a87ccc58eb755a4c10847846791
Lino Leisdier Alfonso Morales <lleisdier.alfonso@gmail.com>
2024-12-09 18:40:07 -0600

    update

--- ambit_energy_scraper/run.sh
+++ ambit_energy_scraper/run.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
-set -e
+#!/usr/bin/with-contenv bashio
 
-# Run the main Python script
-python3 /app/ambit_energy_scraper.py
+# Extraer valores de configuración
+USERNAME=$(bashio::config 'username')
+PASSWORD=$(bashio::config 'password')
+HA_URL=$(bashio::config 'ha_url')
+HA_TOKEN=$(bashio::config 'ha_token')
+
+bashio::log.info "Iniciando Ambit Energy Scraper..."
+bashio::log.info "Enviando datos a Home Assistant en $HA_URL"
+
+# Ejecutar el script de Python con las configuraciones
+python3 /app/ambit_energy_scraper.py --username "$USERNAME" --password "$PASSWORD" --ha-url "$HA_URL" --ha-token "$HA_TOKEN"
 
